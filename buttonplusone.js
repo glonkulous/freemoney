296e2ab13ed8291263ef0fbef2d48a3a5ff14bfa
glonkulous <alvar.nyman@elev.ga.ntig.se>
2023-05-04 13:08:20 +0200

    123123

--- buttonplusone.js
+++
@@ -1,7 +0,0 @@
-var money = 1;
-document.getElementById("money").innerHTML = String(money)
-
-function plusone(); {
-    money = money+1
-    document.getElementById("money").innerHTML = String(money)
-}
